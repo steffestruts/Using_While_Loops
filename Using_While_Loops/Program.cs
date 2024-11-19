a945ced9186c41199e74367a462b51a157a494fa
Stefan Sjöberg <bird.nerd@outlook.com>
2024-11-19 15:16:13 +0100

    Add project files.

---
+++ Using_While_Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿bool connected = true;
+int number = 0;
+
+while (number < 5)
+{
+    Console.Clear();
+    Console.WriteLine("number = " + number);
+    for (int i = 0; i < 5000; i++)
+    {
+        Console.WriteLine("i = " + i);
+    }
+    if (number == 3)
+        break;
+    number++;
+}
+
+Console.WriteLine("All done!");
